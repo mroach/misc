91d9638db67532433ce85b6edccda492fa88ec65
mroach <mroach@mroach.com>
2017-02-08 10:52:46 +0700

    tmux: auto start guard and rails console

--- shell_setups/tmux_setups.sh
+++ shell_setups/tmux_setups.sh
@@ -1,9 +1,23 @@
 function dev-tmux() {
+    # create a session name based on the dirname. remove chars not valid for a session name
     _session_name=$(echo `basename $PWD` | sed -E "s/[^a-z0-9]+/_/g")
-    tmux new-session -d -s "$_session_name"
-    tmux split-window -h
+    tmux set-option remain-on-exit on
+
+    # create a new session. run guard if available
+    tmux new-session -d -s "$_session_name" 'test -e Guardfile && bundle exec guard'
+
+    # create a new window, run the rails console if available
+    tmux split-window -h 'test -e bin/rails && bundle exec rails c'
+
+    # and then create one more window and leave it blank
     tmux split-window -v
+
+    # create a new window and run tig (text interface to git)
     tmux new-window 'tig'
+
+    # switch back to the first window
     tmux select-window -t :0
+
+    # and re-attach the session now that it's all setup
     tmux -2 attach-session -t "$_session_name"
 }
