cc55be6ae2d4d36aaad9226152aa55093e5fde5d
mroach <mroach@mroach.com>
2017-02-09 10:27:54 +0700

    gnupg: key caching with gpg-agent

--- shell_setups/gpg.sh
+++ shell_setups/gpg.sh
@@ -1,2 +1,9 @@
 # Ensure GNU Privacy Guard will work
-export GPG_TTY=$(tty)
+# The agent will cache the key so you're not prompted constantly for the passphrase
+which gpg-agent &>/dev/null
+
+if [ $? -eq 0 ]; then
+  export GPG_AGENT_INFO=$HOME/.gnupg/S.gpg-agent
+  export GPG_TTY=$(tty)
+  gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
+fi
