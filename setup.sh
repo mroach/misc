08d0730ecb5a5bd7908d7e2a45ebc27b6c6ed032
mroach <mroach@mroach.com>
2017-02-20 11:32:41 +0700

    feat: Script to setup symlinks on a new system

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sets-up symlinks for all the goodies in this repo!
+
+# get 'root' dir by finding the dir this script is in
+# this will allow us to create symlinks with full paths
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $SRCDIR/dotfiles/.gnupg $HOME/.gnupg
+ln -s $SRCDIR/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $SRCDIR/dotfiles/.grc.bashrc $HOME/.grc.bashrc
+ln -s $SRCDIR/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $SRCDIR/dotfiles/.zshrc $HOME/.bashrc
+ln -s $SRCDIR/dotfiles/.zshrc $HOME/.zshrc
+
+# .bash_profile is executed for login shells; .bashrc is executed for interactive non-login shells
+# ensure both are using the same configuration
+ln -s $HOME/.bashrc $HOME/.bash_profile
