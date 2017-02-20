2727aef9462aa78f4f66bb135b02da51902081cf
mroach <mroach@mroach.com>
2017-02-20 11:33:40 +0700

    fix: gpg agent

--- shell_setups/gpg.sh
+++ shell_setups/gpg.sh
@@ -2,8 +2,16 @@
 # The agent will cache the key so you're not prompted constantly for the passphrase
 which gpg-agent &>/dev/null
 
+if [ $? -ne 0 ]; then; exit; fi
+
+export GPG_TTY=$(tty)
+
+pgrep -q gpg-agent
 if [ $? -eq 0 ]; then
-  export GPG_AGENT_INFO=$HOME/.gnupg/S.gpg-agent
-  export GPG_TTY=$(tty)
-  gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
+  source $HOME/.gpg-agent-info
+  export GPG_AGENT_INFO
+  gpg-connect-agent /bye &>/dev/null
+else
+  gpg-agent --daemon &>/dev/null
+  export GPG_AGENT_INFO
 fi
