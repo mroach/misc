964c363f1ccea408465c5d4824e804748ddb4785
mroach <mroach@mroach.com>
2017-03-12 11:39:04 +0700

    feat: Store Sublime Text settings here

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # get 'root' dir by finding the dir this script is in
 # this will allow us to create symlinks with full paths
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+platform=$(uname -s)
 
 ln -s $srcdir/dotfiles/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 ln -s $srcdir/dotfiles/.gitconfig $HOME/.gitconfig
@@ -17,3 +18,7 @@ ln -s $srcdir/dotfiles/.gemrc $HOME/.gemrc
 # .bash_profile is executed for login shells; .bashrc is executed for interactive non-login shells
 # ensure both are using the same configuration
 ln -s $HOME/.bashrc $HOME/.bash_profile
+
+if [ $platform == "Darwin" ]; then
+  ln -s $srcdir/settings/sublime/Preferences.sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+fi
