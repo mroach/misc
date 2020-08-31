073d24de4f0158de996ef6fa56beef840a259346
Michael Roach <michael.roach@new-work.se>
2020-08-31 11:06:52 +0200

    bitbar doh: add vpn and captive tests

--- bitbar/auto_doh.sh
+++ bitbar/auto_doh.sh
@@ -9,11 +9,39 @@
 # <bitbar.dependencies>bash</bitbar.dependencies>
 # <bitbar.abouturl></bitbar.abouturl>
 
+# Pre-requisites
+# --------------
+#
+# macOS can't use DNS over HTTPS natively, so a proxy is required.
+# Cloudflare provides one and it's easy to setup:
+#
+#     brew cask install cloudflare/cloudflare/cloudflared
+#     sudo cloudflared service install
+#
+# You should now have a DNS server listening locally. Check it with:
+#
+#     dig cloudflare.com @127.0.0.1
+#
+# One this is running, you're ready to start using this script.
+#
+# Features
+# --------
+# * If an `openvpn` process is running, don't modify DNS
+# * If current Wi-Fi network is using a captive portal, don't modify DNS
+#
+#
+# Current limitations
+# -------------------
+#
+# * Only manages on device/service at a time, and only Wi-Fi
+# * Doesn't refresh immediately when changing Wi-Fi networks
+
 device=en0
 doh_host=127.0.0.1
 test_host=cloudflare.com
 config_dir=$HOME/.config/auto_doh
 trusted_file=$config_dir/trusted.csv
+captive_test_url=http://captive.apple.com/hotspot-detect.html
 
 test -d $config_dir || mkdir -p $config_dir
 test -f $trusted_file || touch $trusted_file
@@ -41,8 +69,29 @@ current_dns_servers() {
   scutil --dns | grep nameserver | awk -F" : " '{print $2}' | sort | uniq
 }
 
-reset_dns() {
-  networksetup -setdnsservers $service empty
+maybe_remove_custom_dns() {
+  if [ "$(current_dns_servers)" == "$doh_host" ]; then
+    networksetup -setdnsservers $service empty
+  fi
+}
+
+is_any_vpn_active() {
+  answer="no"
+  pgrep -q openvpn && answer="yes"
+  # TODO: Add more VPN checks like native macOS VPN
+  echo $answer
+}
+
+is_captive_state() {
+  answer="no"
+  status=$(curl -w '%{http_code}' -o /dev/null -s $captive_test_url)
+  case $status in
+    3??)
+      answer="yes" ;;
+    *)
+      answer="no" ;;
+  esac
+  echo $answer
 }
 
 service=$(get_device_service "$device")
@@ -50,6 +99,8 @@ current_ssid=$(networksetup -getairportnetwork $device | awk -F": " '{print $2}'
 is_trusted=false
 colour=red
 local_dns_alive=false
+any_vpn=$(is_any_vpn_active)
+captive=$(is_captive_state)
 
 for ssid in "${trusted_networks[@]}"; do
   if [[ "$ssid" == "$current_ssid" ]]; then
@@ -59,39 +110,45 @@ for ssid in "${trusted_networks[@]}"; do
   fi
 done
 
-echo -n "DoH "
-[ "$is_trusted" == true ] && echo " ⇢" || echo " ↩︎"
+echo -n "DoH"
+
+if [ "$any_vpn" == "yes" ]; then
+  echo " ⏸ "
+else
+  [ "$is_trusted" == true ] && echo " ⇢" || echo " ↩︎"
+fi
 
 echo "---"
 echo "$current_ssid | color=$colour"
 
+# Sub-menu for the current network
 if [ "$is_trusted" == true ]; then
   echo "-- Untrust this network | bash='$0' param1=del-trusted param2='$current_ssid' terminal=false refresh=true"
 else
   echo "-- Trust this network | bash='$0' param1=add-trusted param2='$current_ssid' terminal=false refresh=true"
 fi
 
-dig +short $test_host @$doh_host &>/dev/null
-if [ $? -eq 0 ]; then
-  local_dns_alive=true
-else
-  echo "DoH @$doh_host is DOWN | color=red"
-  reset_dns
-fi
-
-if [ "$is_trusted" == false ]; then
-  if [ "$local_dns_alive" == true ]; then
+if [ "$any_vpn" == "yes" ]; then
+  echo "VPN active. Not modifying DNS. | color=red"
+  maybe_remove_custom_dns
+elif [ "$captive" == "yes" ]; then
+  echo "Captive network detected. Not modifying DNS. | color=red"
+  maybe_remove_custom_dns
+elif [ "$is_trusted" == false ]; then
+  dig +short $test_host @$doh_host &>/dev/null
+  if [ $? -eq 0 ]; then
     echo ":lock: Using DoH @$doh_host"
     networksetup -setdnsservers $service $doh_host
   else
+    echo "DoH @$doh_host is down | color=red"
     echo "Won't use unresponsive $doh_host | color=red"
+    maybe_remove_custom_dns
   fi
 else
   echo "Using network-provided DNS"
-  reset_dns
+  maybe_remove_custom_dns
 fi
 
-echo
 echo "DNS Servers"
 for addr in $(current_dns_servers); do
   echo "$addr | color=blue"
