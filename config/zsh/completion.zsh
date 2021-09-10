a0315521cd1b8ee7c9ca5d991649b63ea7ae6640
mroach <git@c.mroach.com>
2021-09-10 17:07:53 +0200

    config(zsh): load completion from "gh" if available

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -2,6 +2,10 @@ if command -v kubectl &>/dev/null ; then
   source <(kubectl completion zsh)
 fi
 
+if command -v gh &>/dev/null; then
+  source <(gh completion --shell zsh)
+fi
+
 if [ -f /usr/share/google-cloud-sdk/completion.zsh.inc ] ; then
   source /usr/share/google-cloud-sdk/completion.zsh.inc
 fi
