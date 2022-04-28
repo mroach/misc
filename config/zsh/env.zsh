ddd01be9b9601843b16f7be92011568426005b93
mroach <git@c.mroach.com>
2022-04-28 07:27:08 +0200

    config(env): add ~/.cargo/bin to PATH

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -6,5 +6,8 @@ export PATH=$HOME/.local/bin:/opt/local/bin:/opt/local/sbin:$PATH
 # Go
 export PATH=$PATH:$HOME/go/bin
 
+# Rust
+export PATH=$PATH:$HOME/.cargo/bin
+
 # ejson private keys
 export EJSON_KEYDIR=$HOME/.local/share/ejson/keys
